b03cc146b8ce35bb4487e07a4a42e2e47e0caa76
romulosilvestre <romulo.silvestre@df.estudante.senai.br>
2022-06-24 15:19:15 -0300

    alo mundo funcionando

---
+++ src/br/com/senaitaguatinga/aprendiz/Principal.java
@@ -0,0 +1,11 @@
+package br.com.senaitaguatinga.aprendiz;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Alô Mundo!");
+
+	}
+
+}
